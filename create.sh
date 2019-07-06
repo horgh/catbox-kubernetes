a4cd49a25a6014e14b45570e5b2363d77f4cfa50
Will Storey <will@summercat.com>
2019-07-06 12:12:27 -0700

    Flesh out configuration

--- create.sh
+++ create.sh
@@ -12,5 +12,7 @@ perl -p -i -e "s/loadBalancerIP: .*/loadBalancerIP: $IP/" resources.yml
 kubectl create secret generic servers-config --from-file=servers.conf
 kubectl create configmap      certificate    --from-file=certificate.pem
 kubectl create secret generic tls-key        --from-file=key.pem
+kubectl create secret generic opers-config   --from-file=opers.conf
+kubectl create secret generic users-config   --from-file=users.conf
 
 kubectl apply -f resources.yml
