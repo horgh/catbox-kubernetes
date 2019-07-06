69599f316d39045cd4e8d130d477d208a57cacb1
Will Storey <will@summercat.com>
2019-07-06 11:25:17 -0700

    Add program that automates creation

---
+++ create.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+gcloud container clusters create my-cluster --machine-type=g1-small --num-nodes=2
+
+gcloud compute addresses create irc-ip --region us-west1
+
+IP=$(gcloud compute addresses list --format='value(address)' --limit=1)
+perl -p -i -e "s/loadBalancerIP: .*/loadBalancerIP: $IP/" resources.yml
+
+kubectl create secret generic servers-config --from-file=servers.conf
+kubectl create configmap      certificate    --from-file=certificate.pem
+kubectl create secret generic tls-key        --from-file=key.pem
+
+kubectl apply -f resources.yml
