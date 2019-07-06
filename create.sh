cba306141c2e2f28f838b1a8d22ec3b28c9d1d0b
Will Storey <will@summercat.com>
2019-07-06 12:30:46 -0700

    Put all configs in one secret volume

--- create.sh
+++ create.sh
@@ -9,10 +9,13 @@ gcloud compute addresses create irc-ip --region us-west1
 IP=$(gcloud compute addresses list --format='value(address)' --limit=1)
 perl -p -i -e "s/loadBalancerIP: .*/loadBalancerIP: $IP/" resources.yml
 
-kubectl create secret generic servers-config --from-file=servers.conf
-kubectl create configmap      certificate    --from-file=certificate.pem
-kubectl create secret generic tls-key        --from-file=key.pem
-kubectl create secret generic opers-config   --from-file=opers.conf
-kubectl create secret generic users-config   --from-file=users.conf
+kubectl create secret generic irc-config \
+	--from-file=configs/catbox-0.conf \
+	--from-file=configs/catbox-1.conf \
+	--from-file=configs/certificate.pem \
+	--from-file=configs/key.pem \
+	--from-file=configs/opers.conf \
+	--from-file=configs/servers.conf \
+	--from-file=configs/users.conf
 
 kubectl apply -f resources.yml
